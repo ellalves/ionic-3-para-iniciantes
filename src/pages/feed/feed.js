14e4b4a54d9514a7c52c24ba2929e1bbc25babdb
ellalves <ellalves@hotmail.com.br>
2017-10-25 15:47:36 -0300

    first commit

---
+++ src/pages/feed/feed.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'page-feed h2feed_title': {
+    'color': 'red',
+    'fontWeight': '700'
+  },
+  'page-feed pfeed_date': {
+    'color': 'green'
+  }
+});
---
+++ src/pages/intro/intro.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'page-intro slide_image img': {
+    'width': [{ 'unit': 'px', 'value': 240 }]
+  }
+});
